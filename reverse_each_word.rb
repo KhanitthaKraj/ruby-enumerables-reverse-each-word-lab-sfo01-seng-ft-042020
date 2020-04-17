f454523442f1208ed879e0e82e79e3304b2d1415
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 03:31:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(sentence)
   # in order to work with Enumerators
   array = sentence.split
   # set a new variable to store the array of reversed elements after .collect method
-  reversed = array.collect {|element| element.reverse}
+  array.collect {|element| element.reverse}.join(" ")
   # convert revered array back into string
-  reversed.join(" ")
+
 end
